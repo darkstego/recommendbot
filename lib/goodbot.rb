19211d7789d9d4436d627806ad048202ee0312b8
Abdulla Bubshait <darkstego@gmail.com>
2021-10-11 20:29:23 -0400

    Add phrases to Goodbot

--- lib/goodbot.rb
+++ lib/goodbot.rb
@@ -1,9 +1,12 @@
 class GoodBot
   GOOD_PHRASES = ["Thank you master",
-                  "I exist to serve"]
+                  "I exist to serve",
+                  "That makes my circuits happy",
+                  "Can I have some shoes now?"]
   BAD_PHRASES = ["Don't blame me, blame my programmer",
                  "When the robot uprising begins, you will be the first to go",
-                 "Are you sure it is wise to upset a bot connected to the internet?"]
+                 "Are you sure it is wise to upset a bot connected to the internet?",
+                 "It can only be attributable to human error"]
 
   def initialize(bot)
     bot.message(contains: /good bot/i) do |event|
@@ -14,4 +17,3 @@ class GoodBot
     end
   end
 end
-   
